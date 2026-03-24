b7c5381c1bcb87806478bb53be3be4f107e36230
Omoju-Mayowa <omojumayowa@gmail.com>
2026-03-24 19:16:14 +0100

    fix: fixed cors issues

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ app.use(generalLimiter)
 // CORS
 const allowedOrigins = [
     'https://corper-compass-frontend.vercel.app',
+    'https://corper-compass-frontend.vercel.app/',
     'https://group-11-corpercompass-frontend-sb9.vercel.app',
     'https://group-11-corpercompass-frontend-sb9.vercel.app/',
     process.env.FRONTEND_URL || 'http://localhost:5500',
